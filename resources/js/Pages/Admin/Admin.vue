41394ebdb1bffd47b2103dee27779d0356a462a2
V <V@MacBook-Pro-de-V.local>
2022-03-09 10:02:09 +0100

    created files for each user profile :art:

---
+++ resources/js/Pages/Admin/Admin.vue
@@ -0,0 +1,16 @@
+<template>
+    
+</template>
+
+<script>
+import { Head, Link } from '@inertiajs/inertia-vue3';
+export default {
+    props:{
+        productos: Array
+    },
+    components:{
+        Head,
+        Link,
+    }
+};
+</script>
