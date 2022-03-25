dcb101a34be01d45646735ce681a5116e334bec0
V <V@MacBook-Pro-de-V.local>
2022-03-25 11:50:47 +0100

    removed redundant code :coffin:

--- resources/js/Pages/Assigned.vue
+++ resources/js/Pages/Assigned.vue
@@ -5,7 +5,7 @@
             <Link
                 :href="route('admin')"
                 class=" mr-2.5 font-bold text-amarillo rounded-lg text-sm py-2.5 text-center flex items-center">
-                <svg class=" fill-amarillo mr-1 ml-2 -mr-1 w-5 h-5 rotate-180" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <svg class=" fill-amarillo mr-1 ml-2 w-5 h-5 rotate-180" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 Control de Roles</Link
                         >
