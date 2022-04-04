1c7476ae9f30245b473989c95ea0004564659405
UxerDevCoding <omarheviaarbana@gmail.com>
2022-04-04 15:30:08 +0200

    DeleteBTn component modified and title font weight changed to bold

--- resources/js/Jetstream/Title.vue
+++ resources/js/Jetstream/Title.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1 class="mb-6 text-xl font-semibold text-center text-azul">
+    <h1 class="mb-6 text-xl font-bold text-center text-azul">
       <slot></slot>
     </h1>
 </template>
