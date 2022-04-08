c03c6dcfcfa800dfc3778f1dc35ec8de862601f6
franjavi01 <fjsg0001@gmail.com>
2022-04-08 14:34:35 +0200

    temporally changes for make Progress Bar

---
+++ resources/js/Jetstream/ProgressBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="w-3/4 mx-auto lg:w-2/5 flex-column">
+      <div
+        class="bg-amarillo flex items-center bg-white justify-center px-10 py-6 mb-12 border-4 rounded-xl border-amarillo drop-shadow-md hover:drop-shadow-2xl"
+      >
+      <div class="text-white">
+      <h1>Tareas completadas</h1>
+      </div>
+            <div class="w-full bg-gray-200 rounded-full">
+  <div class="bg-azul text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-l-full px-4" style="width: 50%">50%</div>
+</div>
+         <slot></slot>
+        </div>
+      </div>
+</template>
+
+<script>
+    
+</script>
