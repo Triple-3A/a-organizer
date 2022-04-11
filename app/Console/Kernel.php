247bb9384c3d38e13bf993ae94c2c70cb695ef7f
Raul <raulfernan000@gmail.com>
2022-04-11 14:31:22 +0200

    added schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Models\Task;
+
 
 class Kernel extends ConsoleKernel
 {
@@ -15,7 +17,13 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        // $schedule->command('migrate:fresh --seed')->everyMinute();
+        $schedule->call(function () {
+            $tasks = Task::get();
+            foreach ($tasks as $task) {
+                $task->update(["done" => true]);
+            }
+        })->everyDay();
     }
 
     /**
